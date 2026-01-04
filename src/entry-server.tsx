ebf014b659345fd0f20aa44b3a18bd57c2e77fb9
Gabriel Ganeles <ganeles@campus.technion.ac.il>
2026-01-04 13:03:45 +0200

    first commit

---
+++ src/entry-server.tsx
@@ -0,0 +1,51 @@
+// @refresh reload
+import { createHandler, StartServer } from "@solidjs/start/server";
+
+import { initializeApp } from "firebase/app";
+
+
+export default createHandler(() => (
+    <StartServer
+        document={({ assets, children, scripts }) => (
+            <html lang="en">
+                <head>
+                    <meta charset="utf-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <link rel="icon" href="/favicon.ico" />
+                    {assets}
+                </head>
+                <body>
+                    <div id="app">{children}</div>
+                    {scripts}
+                </body>
+            </html>
+        )}
+    />
+));
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
